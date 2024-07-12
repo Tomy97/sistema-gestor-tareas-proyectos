9c1fab10164b6a0b36cbb5b072e4b36495d9338a
Tomy97 <tomas.pandu@gmail.com>
2024-07-12 06:11:53 -0300

    estilos de background de color para las cards del dashboard

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -7,8 +7,13 @@ interface ProjectCardProps {
 
 export const ProjectCard = ({ project }: ProjectCardProps) => {
   return (
-    <Card title={project.name} className='text-primario'>
-      sadjandasdas
+    <Card title={project.name} className="text-primario relative test-title">
+      <div className='ml-6'>
+          <span className="font-semibold">
+            Fecha límite: {new Date(project.date).toLocaleDateString()}
+          </span>
+      </div>
+      <div className="background-border-color" style={{ backgroundColor: project.color }}></div>
     </Card>
   )
 }
